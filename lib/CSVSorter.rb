c57d8339cf0966f12ce559fd5f6d26feffaded82
NathanKolbas <nathan2000@outlook.com>
2019-10-15 02:48:39 -0500

    Added methods and a test

--- lib/CSVSorter.rb
+++ lib/CSVSorter.rb
@@ -1,6 +1,40 @@
-require "CSVSorter/version"
+require 'csv'
 
 module CSVSorter
-  class Error < StandardError; end
-  # Your code goes here...
+  class Methods
+    @file_path
+    
+    def run
+      orignial_file = get_csv_file
+      sorted_file = sort_last_first(orignial_file)
+      output_csv_file(sorted_file, @file_path)
+    end
+
+    #Read CSV file into an array
+    def get_csv_file
+      print 'Please enter the file location of the CSV you would like to sort: '
+      @file_path = gets.chomp
+
+      #Simple error check if the file is CSV
+      if !@file_path.include? '.csv'
+        print "\nPlease enter a CSV file"
+        nil
+      else
+        CSV.parse(File.read(@file_path), headers: true)
+      end
+    end
+
+    #Write data to CSV file
+    def output_csv_file(csv_array, file_path)
+      CSV.open(file_path, "w") do |csv|
+        csv << ['First Name', 'Last Name', 'Zip Code']
+        csv_array.each { |r| csv << r } 
+      end
+    end
+
+    #Sort CSV file by last name and then first name
+    def sort_last_first(csv_array)
+      csv_array.sort_by { |t| [t['Last Name'].downcase, t['First Name'].downcase] }
+    end
+  end
 end
--- spec/CSVSorter_spec.rb
+++ spec/CSVSorter_spec.rb
@@ -1,9 +1,14 @@
-RSpec.describe CSVSorter do
-  it "has a version number" do
-    expect(CSVSorter::VERSION).not_to be nil
-  end
+require 'CSVSorter'
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe CSVSorter::Methods do
+  subject { CSVSorter::Methods.new }
+
+  describe '.sort_last_first' do
+    let(:unsorted_csv) { CSV.parse(File.read('spec\unsorted.csv'), headers: true) }
+    let(:sorted_csv) { CSV.parse(File.read('spec\sorted.csv'), headers: true) }
+
+    it 'returns a sorted array' do
+      expect(subject.sort_last_first(unsorted_csv).map(&:to_h)).to eq(sorted_csv.map(&:to_h))
+    end
   end
 end
