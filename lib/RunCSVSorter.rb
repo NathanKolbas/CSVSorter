d25de410595c6ef10704d9acfde0605939e2dc4e
NathanKolbas <nathan2000@outlook.com>
2019-10-15 21:23:01 -0500

    Typo fix

--- lib/RunCSVSorter.rb
+++ lib/RunCSVSorter.rb
@@ -3,8 +3,8 @@ require_relative 'CSVSorter'
 c = CSVSorter::Methods.new
 print 'Please enter the file path of the CSV file you would like to sort: '
 file_path = gets.chomp
-orignial_file = c.get_csv_file(file_path)
-unless orignial_file.nil? 
-    sorted_file = c.sort_last_first(orignial_file)
+original_file = c.get_csv_file(file_path)
+unless original_file.nil? 
+    sorted_file = c.sort_last_first(original_file)
     c.output_csv_file(sorted_file, file_path)
 end
